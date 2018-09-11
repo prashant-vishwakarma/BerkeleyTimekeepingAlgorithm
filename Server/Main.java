16b1ecb78ac6f5dc17f4537bd35150f4183f23e0
Prashant Vishwakarma <32397516+prashant-vishwakarma@users.noreply.github.com>
2018-09-11 14:14:43 +0530

    Create Main.java

---
+++ Server/Main.java
@@ -0,0 +1,43 @@
+package ui;
+
+import java.net.*;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+import java.io.*;
+
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Berkeley's Server\nEnter Port Number");
+		int port = Integer.parseInt(br.readLine());
+		
+		try {
+			ServerSocket serverSocket = new ServerSocket(port);
+			Socket socket = serverSocket.accept();
+			
+			ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
+			Date date = (Date) objectInputStream.readObject();
+			
+			Date myDate = new Date();
+			
+			System.out.println("Client" + date.toString() + "\n" + myDate.toString());
+			
+			long differenceInSeconds = myDate.getTime() - date.getTime();
+			System.out.println("Difference in Seconds: " + TimeUnit.MILLISECONDS.toSeconds(differenceInSeconds));
+			
+			Date averageDate =new Date((myDate.getTime() + date.getTime())/2);
+			System.out.println("Average Date" + averageDate.toString());
+			
+			ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+			objectOutputStream.writeObject(averageDate);
+			
+			serverSocket.close();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
